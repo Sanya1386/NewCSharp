2e3819c721f5ebbbd4a2186c60ff83998f2ac8d4
Sanya <Sanya-dmb@yandex.ru>
2024-02-12 21:33:02 +0700

    git lesson2:task1

--- lesson2/task1/Program.cs
+++ lesson2/task1/Program.cs
@@ -1,7 +1,19 @@
-﻿int[] number = new int[5];
-number[0] = 5;
-number[1] = 7;
-number[2] = 8;
-number[3] = 6;
-number[4] = 4;
-Console.Writeline(number[0]);
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+int[] arr = new int[5];
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 8;
+arr[3] = 9;
+arr[4] = 6;
+System.Console.WriteLine(arr[0]);
+System.Console.WriteLine(arr[1]);
+System.Console.WriteLine(arr[2]);
+System.Console.WriteLine(arr[3]);
+System.Console.WriteLine(arr[4]);
+
+//System.Console.WriteLine(arr[5]);
+
+int[] arr2 = nenw int[5] { 1, 2, 3, 4, 5};
+
+int[] arr3 = { 1, 2, 3, 4, 5 };
