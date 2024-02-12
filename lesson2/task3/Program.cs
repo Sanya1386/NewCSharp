3a694e944d6b2b37108c20c946b31a20003d508c
Sanya <Sanya-dmb@yandex.ru>
2024-02-12 22:52:30 +0700

    add lesson2:task3:task4

---
+++ lesson2/task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int n = 10;
+int[] arr = { 2, 5, 4, 7, 8, 35, 1, 3, 8, 5 };
+int i = 0;
+
+while (i < n)
+{
+    if(arr[i] % 2 == 0)
+    {
+        System.Console.Write(arr[i] + " ");
+       
+    }
+    i = i + 1;
+}
---
+++ lesson2/task4/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+int n = 5;
+int[] arr = { 2, 4, 7, 3, 9 };
+int i = 0;
+int max = arr[0];
+
+while(i < n)
+{
+    if(arr[i] > max)
+    {
+        max = arr[i];
+    }
+    i = i + 1;
+}
+System.Console.WriteLine(max);
+
+max = arr[0];
+for(int j = 0; j < n; j++)
+{
+    if(arr[j] > max)
+    {
+        max = arr[j];
+    }
+}
+System.Console.WriteLine(max);
+
+max = arr[0];
+foreach(int e in arr)
+{
+    if(e > max)
+    {
+        max = e;
+    }
+}
+System.Console.WriteLine(max);
